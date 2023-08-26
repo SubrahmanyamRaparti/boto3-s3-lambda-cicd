e98c0551a8b2748e6527d5cc450ea6b5f1362b6a
Subrahmanyam <rv.subrahmanyam1@gmail.com>
2023-08-26 17:14:32 +0000

    Added few sample error codes

---
+++ error_narrative_sheet.py
@@ -0,0 +1,12 @@
+error_codes_list = [
+    ['ONLINE125-1', 'BATCH121-8', 'Incorrect name format'],
+    ['ONLINE126-3', 'BATCH124-6', 'Incorrect street name'],
+    ['ONLINE165-4', 'BATCH125-1', 'Invalid characters in the provided input'],
+    ['ONLINE127-6', 'BATCH167-2', 'Invalid phone number'],
+    ['ONLINE625-7', 'BATCH125-9', 'Invalid city name'], 
+    ['ONLINE165-1', 'BATCH132-7', 'Invalid address type'], 
+    ['ONLINE155-4', 'BATCH145-6', 'Age not eligible'], 
+    ['ONLINE135-6', 'BATCH185-5', 'Need zip code'], 
+    ['ONLINE155-7', 'BATCH165-3', 'Email address not provided'], 
+    ['ONLINE736-2', 'BATCH767-1', 'Invalid Phone. Use iPhone as an input']
+]
